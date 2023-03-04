1e105a53363197d192c6a04be02dae773890c4be
José Costa <kosta10alberto@gmail.com>
2023-03-04 03:12:58 +0300

    Dev profile

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 import java.util.*;
 
 @Entity
+@Table(name = "_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
